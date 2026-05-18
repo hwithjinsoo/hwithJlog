e29648588450ac56c4039c6a69f9bf8ef01e7314
hwithjinsoo <hwithjinsoo@gamil.com>
2026-05-18 22:31:19 +0900

    오라클 서버 전용 코드 업로드

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,8 @@ const withMDX = createMDX({});
 
 const nextConfig: NextConfig = {
   pageExtensions: ["ts", "tsx", "md", "mdx"],
+
+  assetPrefix: '/proxy/3000', 
 };
 
 export default withMDX(nextConfig);
